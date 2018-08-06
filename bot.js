50d4b7c7ad5107b4f2e1498823d3552186049705
AdrianoExpextre <34609672+AdrianoExpextre@users.noreply.github.com>
2018-08-06 18:46:05 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -125,6 +125,12 @@ let nw =  `${Guardian.guilds.get(member.guild.id).memberCount}`
 let aus = nw.replace(/0/gi, ':zero:').replace(/1/gi, ':one:').replace(/2/gi, ':two:').replace(/3/gi, ':three:').replace(/4/gi, ':four:').replace(/5/gi, ':five:').replace(/6/gi, ':six:').replace(/7/gi, ':seven:').replace(/8/gi, ':eight:').replace(/9/gi, ':nine:');
 Guardian.guilds.get(member.guild.id).channels.get(sysop.contador).edit({ topic: `<:SysopLogoEMOI:439565791357042700> | ${aus} ${sysop.texto}`})
 
+	
+	let nha =  `${Guardian.guilds.get('475549208225775649').memberCount}`
+    
+let nain = nha.replace(/0/gi, ':zero:').replace(/1/gi, ':one:').replace(/2/gi, ':two:').replace(/3/gi, ':three:').replace(/4/gi, ':four:').replace(/5/gi, ':five:').replace(/6/gi, ':six:').replace(/7/gi, ':seven:').replace(/8/gi, ':eight:').replace(/9/gi, ':nine:');
+Guardian.channels.get('475549208225775649').edit({ topic: `<:SysopLogoEMOI:439565791357042700> | ${nain} ${sysop.texto}`})
+
 
 }
 });
@@ -144,7 +150,13 @@ let nw =  `${Guardian.guilds.get(member.guild.id).memberCount}`
     
 let aus = nw.replace(/0/gi, ':zero:').replace(/1/gi, ':one:').replace(/2/gi, ':two:').replace(/3/gi, ':three:').replace(/4/gi, ':four:').replace(/5/gi, ':five:').replace(/6/gi, ':six:').replace(/7/gi, ':seven:').replace(/8/gi, ':eight:').replace(/9/gi, ':nine:');
 Guardian.guilds.get(member.guild.id).channels.get(sysop.contador).edit({ topic: `<:SysopLogoEMOI:439565791357042700> | ${aus} ${sysop.texto}`})
-	}
+	let nha =  `${Guardian.guilds.get('475549208225775649').memberCount}`
+    
+let nain = nha.replace(/0/gi, ':zero:').replace(/1/gi, ':one:').replace(/2/gi, ':two:').replace(/3/gi, ':three:').replace(/4/gi, ':four:').replace(/5/gi, ':five:').replace(/6/gi, ':six:').replace(/7/gi, ':seven:').replace(/8/gi, ':eight:').replace(/9/gi, ':nine:');
+Guardian.channels.get('475549208225775649').edit({ topic: `<:SysopLogoEMOI:439565791357042700> | ${nain} ${sysop.texto}`})
+	
+
+}
 });
 });
 
