30ad882b43e6b637eebf751366f7613116693c6c
AdrianoExpextre <34609672+AdrianoExpextre@users.noreply.github.com>
2018-08-06 18:51:01 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -129,7 +129,7 @@ Guardian.guilds.get(member.guild.id).channels.get(sysop.contador).edit({ topic: 
 	let nha =  `${Guardian.guilds.get('475549208225775649').memberCount}`
     
 let nain = nha.replace(/0/gi, ':zero:').replace(/1/gi, ':one:').replace(/2/gi, ':two:').replace(/3/gi, ':three:').replace(/4/gi, ':four:').replace(/5/gi, ':five:').replace(/6/gi, ':six:').replace(/7/gi, ':seven:').replace(/8/gi, ':eight:').replace(/9/gi, ':nine:');
-Guardian.channels.get('475549208225775649').edit({ topic: `<:SysopLogoEMOI:439565791357042700> | ${nain} ${sysop.texto}`})
+Guardian.guilds.get('459794576627073024').channels.get('475549208225775649').edit({ topic: `<:SysopLogoEMOI:439565791357042700> | ${nain} ${sysop.texto}`})
 
 
 }
@@ -153,7 +153,7 @@ Guardian.guilds.get(member.guild.id).channels.get(sysop.contador).edit({ topic: 
 	let nha =  `${Guardian.guilds.get('475549208225775649').memberCount}`
     
 let nain = nha.replace(/0/gi, ':zero:').replace(/1/gi, ':one:').replace(/2/gi, ':two:').replace(/3/gi, ':three:').replace(/4/gi, ':four:').replace(/5/gi, ':five:').replace(/6/gi, ':six:').replace(/7/gi, ':seven:').replace(/8/gi, ':eight:').replace(/9/gi, ':nine:');
-Guardian.channels.get('475549208225775649').edit({ topic: `<:SysopLogoEMOI:439565791357042700> | ${nain} ${sysop.texto}`})
+Guardian.guilds.get('459794576627073024').channels.get('475549208225775649').edit({ topic: `<:SysopLogoEMOI:439565791357042700> | ${nain} ${sysop.texto}`})
 	
 
 }
