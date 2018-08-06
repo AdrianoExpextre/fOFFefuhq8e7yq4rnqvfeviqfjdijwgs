1fef043c44fd4d19ecfe078bde6e4c782151ccd3
AdrianoExpextre <34609672+AdrianoExpextre@users.noreply.github.com>
2018-08-06 17:30:09 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -113,15 +113,17 @@ Guardian.on('guildMemberAdd', member => {
 database.Guilds.findOne({"_id": member.guild.id}, function(erra, sysop) {
 if (!sysop)  return;
 if (!sysop.autorole) return;	
+if(!sysop.contador) return;
+if(!sysop.texto) return;	
+if(!Guardian.guilds.get(member.guild.id).channels.get(sysop.contador)) return;		
 if (sysop) {	
 member.addRole(sysop.autorole)
 sysop.save();
 	
-//contador
 let nw =  `${Guardian.guilds.get(member.guild.id).memberCount}`
     
 let aus = nw.replace(/0/gi, ':zero:').replace(/1/gi, ':one:').replace(/2/gi, ':two:').replace(/3/gi, ':three:').replace(/4/gi, ':four:').replace(/5/gi, ':five:').replace(/6/gi, ':six:').replace(/7/gi, ':seven:').replace(/8/gi, ':eight:').replace(/9/gi, ':nine:');
-Guardian.channels.get(sysop.contador).edit({ topic: `<:SysopLogoEMOI:439565791357042700> | ${aus} ${sysop.texto}`})
+Guardian.guilds.get(member.guild.id).channels.get(sysop.contador).edit({ topic: `<:SysopLogoEMOI:439565791357042700> | ${aus} ${sysop.texto}`})
 
 
 }
@@ -131,11 +133,16 @@ Guardian.channels.get(sysop.contador).edit({ topic: `<:SysopLogoEMOI:43956579135
 Guardian.on('guildMemberRemove', member => {
 database.Guilds.findOne({"_id": member.guild.id}, function(erra, sysop) {
 //contador
+	
+if(!sysop.contador) return;
+if (!sysop.texto) return;
+if(!Guardian.guilds.get(member.guild.id).channels.get(sysop.contador)) return;	
+if (!sysop) {
 let nw =  `${Guardian.guilds.get(member.guild.id).memberCount}`
     
 let aus = nw.replace(/0/gi, ':zero:').replace(/1/gi, ':one:').replace(/2/gi, ':two:').replace(/3/gi, ':three:').replace(/4/gi, ':four:').replace(/5/gi, ':five:').replace(/6/gi, ':six:').replace(/7/gi, ':seven:').replace(/8/gi, ':eight:').replace(/9/gi, ':nine:');
-Guardian.channels.get(sysop.contador).edit({ topic: `<:SysopLogoEMOI:439565791357042700> | ${aus} ${sysop.texto}`})
-
+Guardian.guilds.get(member.guild.id).channels.get(sysop.contador).edit({ topic: `<:SysopLogoEMOI:439565791357042700> | ${aus} ${sysop.texto}`})
+	}
 });
 });
 
