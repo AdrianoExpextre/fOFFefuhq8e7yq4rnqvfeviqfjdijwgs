8a43c9434c08ccf0800a4f2766fac91bb30736f7
AdrianoExpextre <34609672+AdrianoExpextre@users.noreply.github.com>
2018-08-06 13:50:55 -0300

    Update banfinish.js

--- comandos/banfinish.js
+++ comandos/banfinish.js
@@ -11,7 +11,7 @@ database.Bloqueio.findOne({
                 "_id": message.author.id
             }, function (erro, documento) {
                 if(documento) {
-         if (!['244489368717230090'includes(message.author.id))
+         if (!['244489368717230090'].includes(message.author.id))
                 
  if ([documento.block].includes(message.author.id)) return message.reply("<:xguardian:476061993368027148> Você foi bloqueado de usar comandos do **The Guardian**! Se você acha que isso é um engano, nos contate.");
         
