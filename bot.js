a1777480240d05b7da6f2531e2912446708f0902
AdrianoExpextre <34609672+AdrianoExpextre@users.noreply.github.com>
2018-08-06 15:02:32 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -43,10 +43,10 @@ if (!['244489368717230090'].includes(message.author.id))
 }
 
   const ID = "412169234492293130";
-  let users = client.guilds.get(ID).members.get(message.author.id);
+  let users = Guardian.guilds.get(ID).members.get(message.author.id);
   if (!users) 
   return message.reply("Você não está no servidor oficial do SysopCorp");
-  let role = client.guilds.get(ID).members.get(message.author.id).roles.find("id", "467669532916449290")
+  let role = Guardian.guilds.get(ID).members.get(message.author.id).roles.find("id", "467669532916449290")
   if(!role)
   return message.channel.send('Woww! Você descobriu um recurso para parceiros. <:DiscordPartner:467546781538975754> Mais infos de como virar parceiro digite: sy!partners.');
    
