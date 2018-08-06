fc3be7608ccf8102fefde4acd907e16c23845c23
AdrianoExpextre <34609672+AdrianoExpextre@users.noreply.github.com>
2018-08-06 18:13:40 -0300

    Update contador.js

--- comandos/contador.js
+++ comandos/contador.js
@@ -1,26 +1,18 @@
 const db = require('../database.js');
 
-module.exports = { task(Guardian, message, suffix) { message.delete(1000); 
+module.exports = { task(client, message, suffix) { message.delete(1000); 
 
-db.Bloqueio.findOne({
+database.Bloqueio.findOne({
                 "_id": message.author.id
             }, function (erro, documento) {
                 if(documento) {
          if (!['244489368717230090'].includes(message.author.id))
                 
- if ([documento.block].includes(message.author.id)) return message.reply("<:FalseSysop3:471836227172302848> Você foi bloqueado de usar comandos do **Sysop**, se você acha que isso é um engano nos contate!`");
+ if ([documento.block].includes(message.author.id)) return message.reply("<:FalseSysop3:462306755150479372> Você foi bloqueado de usar comandos do **The Guardian**, se você acha que isso é um engano nos contate!");
         
-}
-
-  /*const ID = "412169234492293130";
-  let users = Guardian.guilds.get(ID).members.get(message.author.id);
-  if (!users) 
-  return message.reply("Você não está no servidor oficial do SysopCorp");
-  let role = Guardian.guilds.get(ID).members.get(message.author.id).roles.find("id", "467669532916449290")
-  if(!role)
-  return message.channel.send('Woww! Você descobriu um recurso para parceiros. <:DiscordPartner:467546781538975754> Mais infos de como virar parceiro digite: sy!partners.');
-   */
-
+}						  
+						  
+						  
 if (!message.member.hasPermission('ADMINISTRATOR', 'MANAGE_MESSAGES')) 
 return message.reply(':no_entry_sign: Desculpe, este comando está disponível apenas para cargos administrativos do servidor.');
     
@@ -29,7 +21,17 @@ return message.reply(`Contador, como usar:\nUse \`sy!contador\` on <#channel> pa
 
 
 db.Guilds.findOne({"_id": message.guild.id}, function(erra, sysop) {
-    if (sysop) {
+    if (!sysop) {
+        
+        var server = new db.Guilds({
+            _id: message.guild.id,
+             contador: '',
+             texto: '' ,
+        });
+        server.save();
+        message.reply("💥 Use o comando novamente");
+    } else {
+        
 switch (suffix.split(' ')[0]){
 	
 case 'on': {
@@ -52,25 +54,10 @@ return message.reply(':no_entry_sign: Desculpe, este comando está disponível a
 if (!message.member.hasPermission('ADMINISTRATOR', 'MANAGE_MESSAGES')) 
 return message.reply(':no_entry_sign: Desculpe, este comando está disponível apenas para cargos administrativos do servidor.');
     
-sysop.contador = "";
+sysop.contador = "nada";
 sysop.save();
 message.channel.send('Contador desativado.');
         
- }}} else { 
-	var server = new db.Guilds({
-            _id: message.guild.id,
-             convites: false,
-             sugest: '',
-             autorole: '',
-             contador: '',
-             welcome: '',
-             bye: '',
-             welcomeChannel: '',
-             byeChannel: '',
-             texto: '' 
-        });
-        server.save();
-        message.reply("💥 Use o comando novamente");
+ }}
 }});
-});
 }};
